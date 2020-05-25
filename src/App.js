77114999f7fb978ed3121cca0d68cd0b221034c8
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-05-24 23:30:48 -0500

    (Add): Sidebar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Routes, Route } from 'react-router';
 import { Orders } from './components/pages/Orders';
 import { Menu } from './components/pages/Menu';
 import { NewMeal } from './components/pages/NewMeal';
+import { Sidebar } from './components/ui/Sidebar';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Orders />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/nuevo-platillo" element={<NewMeal />} />
-      </Routes>
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-3/5 xl:w-4/5">
+        <Routes>
+          <Route exact path="/" element={<Orders />} />
+          <Route exact path="/menu" element={<Menu />} />
+          <Route exact path="/nuevo-platillo" element={<NewMeal />} />
+        </Routes>
+      </div>
     </div>
   );
 }
