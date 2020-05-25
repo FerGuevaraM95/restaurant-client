61b5cdc96f9529c3292921ff9293035a15e875f7
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-05-24 22:06:51 -0500

    (Add): Create routes from pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router';
 
 import { Orders } from './components/pages/Orders';
+import { Menu } from './components/pages/Menu';
+import { NewMeal } from './components/pages/NewMeal';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Orders />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/nuevo-platillo" element={<NewMeal />} />
       </Routes>
     </div>
   );
