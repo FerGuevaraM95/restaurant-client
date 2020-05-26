ab4fc0667bf8932f05f928a2b1d3896ffe30267d
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-05-26 00:04:48 -0500

    (Add): Form to NewMeal component

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Menu = () => {
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Menú</h1>
+      <Link to="nuevo-platillo" className="mb-5 p-2 bg-blue-700 inline-block text-white font-bold uppercase">
+        Agregar platillo
+      </Link>
     </>
   );
 }
--- src/components/pages/NewMeal.js
+++ src/components/pages/NewMeal.js
@@ -4,6 +4,74 @@ export const NewMeal = () => {
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Nuevo Platillo</h1>
+
+      <div className="flex justify-center mt-10">
+        <div className="w-full max-w-3xl">
+          <form>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Nombre</label>
+              <input 
+                id="name" 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Nombre del Platillo"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">Precio</label>
+              <input
+                id="price"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="numer"
+                placeholder="$20"
+                min="0"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">Categoría</label>
+              <select
+                id="category"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="categoria"
+              >
+                <option value="">-- Seleccione --</option>
+                <option value="breakfast">Desayuno</option>
+                <option value="lunch">Comida</option>
+                <option value="dinner">Cena</option>
+                <option value="drinks">Bebidas</option>
+                <option value="desserts">Postres</option>
+                <option value="salads">Ensaladas</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="image">Imagen</label>
+              <input
+                id="image"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="file"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">Descripción</label>
+              <textarea
+                id="description"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40"
+                placeholder="Descripción del Platillo"
+              ></textarea>
+            </div>
+
+            <input
+              type="submit"
+              className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold"
+              value="Agregar Platillo"
+            />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
