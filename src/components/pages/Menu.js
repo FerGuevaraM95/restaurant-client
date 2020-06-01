0109b5fb1682fe3c7b4b2b51afe7eb65749dc947
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-05-31 23:24:29 -0500

    (Add): Snapshot data in realtime

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../../firebase';
 
 export const Menu = () => {
+
+  const [meals, setMeals] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const getMeals = () => {
+      firebase.db.collection('products').onSnapshot(handleSnapshot);
+
+    };
+    getMeals();
+  }, []);
+
+  // Snapshop for database in realtime
+  function handleSnapshot(snapshop) {
+    const meals = snapshop.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      };
+    });
+    setMeals(meals);
+  };
+
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Menú</h1>
