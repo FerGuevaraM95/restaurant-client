27111c42ba89d6f6ea4cdd9053642f225c07a340
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-06-02 00:02:48 -0500

    (Add): Update meal existence

--- src/components/ui/Meal.js
+++ src/components/ui/Meal.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react';
+import { FirebaseContext } from '../../firebase';
 
-export const Meal = ({ meal: { name, image, existence, category, price, description } }) => {
+export const Meal = ({ meal: { id, name, image, existence, category, price, description } }) => {
+
+  // Existence ref
+  const existenceRef = useRef(existence);
+
+  // Firebase context for changes in DB
+  const { firebase } = useContext(FirebaseContext);
+
+  const updateDisponibility = () => {
+    const existence = (existenceRef.current.value === "true")
+    
+    try {
+      firebase.db.collection('products')
+        .doc(id)
+        .update({
+          existence
+        })
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <article className="w-full px-3 mb-4">
@@ -8,6 +29,21 @@ export const Meal = ({ meal: { name, image, existence, category, price, descript
         <div className="lg:flex">
           <div className="lg:w-5/12 xl:w-3/12">
             <img src={image} alt={name} />
+
+            <div className="sm:flex sm:-mx-2 pl-2">
+              <label className="mt-5 sm:w-2/4" htmlFor="">
+                <span className="block text-gray-800 mb-2">Existencia</span>
+
+                <select
+                  value={existence}
+                  ref={existenceRef}
+                  onChange={() => updateDisponibility()}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-white">
+                  <option value="true">Disponible</option>
+                  <option value="false">No Disponible</option>
+                </select>
+              </label>
+            </div>
           </div>
           <div className="lg:w-7/12 xl:w-9/12 pl-5">
             <h1 className="font-bold text-2xl text-yellow-600 mb-4">
