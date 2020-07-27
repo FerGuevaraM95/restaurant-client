b74c5572202bae97803a5de987e7338b6e5954ba
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-07-26 23:50:21 -0500

    (Fix): Link to new meal

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -42,7 +42,7 @@ export const Menu = () => {
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Menú</h1>
-      <Link to="nuevo-platillo" className="mb-5 p-2 bg-blue-700 inline-block text-white font-bold uppercase">
+      <Link to="/nuevo-platillo" className="mb-5 p-2 bg-blue-700 inline-block text-white font-bold uppercase">
         Agregar platillo
       </Link>
 
