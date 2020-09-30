350bd3bba16a578532463bafdbbd717e31044aae
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-09-29 21:44:32 -0500

    (Add): Get orders from firebase

--- src/components/pages/Orders.js
+++ src/components/pages/Orders.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+
+import {FirebaseContext} from '../../firebase';
 
 export const Orders = () => {
+  const {firebase} = useContext(FirebaseContext);
+
+  // Orders
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const getOrders = () => {
+      firebase.db.collection('orders').where('completed', "==", false).onSnapshot(handleSnapshot);
+    }
+    getOrders();
+  }, [])
+
+  function handleSnapshot(snapshot) {
+    const orders = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      };
+    });
+
+    setOrders(orders);
+  };
+
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Ordenes</h1>
